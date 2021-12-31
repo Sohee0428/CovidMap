beb56e7d371feb68f09c7b1ce08d8cf3b0e9b8fb
Sohee0428 <82200065+Sohee0428@users.noreply.github.com>
2021-12-31 20:27:09 +0900

    viewBinding, dataBinding, bindingAdapter 적용 같은 기능끼리 함수로 묶기

--- app/src/main/java/com/autocrypt/covidmap/map/MapActivity.kt
+++ app/src/main/java/com/autocrypt/covidmap/map/MapActivity.kt
@@ -18,80 +18,108 @@ import kotlinx.coroutines.launch
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mapView: MapView
+    private lateinit var binding: ActivityMapBinding
     private lateinit var naverMap: NaverMap
     private val mapViewModel: MapViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_map)
 
-        NaverMapSdk.getInstance(this).client =
-            NaverMapSdk.NaverCloudPlatformClient("2q5r0wb55f")
-
-        mapView = findViewById(R.id.map)
-        mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync(this)
-
+        makeMap(savedInstanceState)
+        makeMarker()
     }
 
     override fun onMapReady(map: NaverMap) {
-        naverMap = map
-        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.5300106, 126.9262812))
-        naverMap.moveCamera(cameraUpdate)
-        getMark(marker)
+        CoroutineScope(Dispatchers.IO).launch {
+            mapViewModel.getAll()
+        }
+        showMap(map)
     }
 
     override fun onStart() {
         super.onStart()
-        mapView.onStart()
+        binding.map.onStart()
     }
 
     override fun onResume() {
         super.onResume()
-        mapView.onResume()
+        binding.map.onResume()
     }
 
     override fun onPause() {
         super.onPause()
-        mapView.onPause()
+        binding.map.onPause()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        mapView.onSaveInstanceState(outState)
+        binding.map.onSaveInstanceState(outState)
     }
 
     override fun onStop() {
         super.onStop()
-        mapView.onStop()
+        binding.map.onStop()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mapView.onDestroy()
+        binding.map.onDestroy()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
-        mapView.onLowMemory()
+        binding.map.onLowMemory()
     }
 
-    private fun getMark(marker: Marker) {
-        marker.icon = OverlayImage.fromResource(R.drawable.ic_baseline_place_24)
-//        marker.position = LatLng(37.5300106, 126.9262812)
-        marker.position = LatLng(mapViewModel.getLatLng(), 126.9262812)
+    private fun makeMap(savedInstanceState: Bundle?) {
+        NaverMapSdk.getInstance(this).client =
+            NaverMapSdk.NaverCloudPlatformClient("2q5r0wb55f")
 
-        mapViewModel.covidCenterLatLng.observe(this) {
-            Log.d("mapLatLng", it.lat)
-            marker.position = LatLng(it.lat.toDouble(), it.lng.toDouble())
-//            marker.position = LatLng(mapViewModel.getLatLng()., it.lng.toDouble())
-        }
-        CoroutineScope(Dispatchers.Main).launch {
-            mapViewModel.getLatLng()
+        binding.map.onCreate(savedInstanceState)
+        binding.map.getMapAsync(this)
+    }
 
-//            marker.position = LatLng(mapViewModel.getLatLng, it.lng.toDouble())
+    private fun showMap(map: NaverMap) {
+        naverMap = map
+        val cameraUpdate = CameraUpdate.scrollAndZoomTo(LatLng(37.5300106, 126.9262812), 12.0)
+        naverMap.moveCamera(cameraUpdate)
+    }
+
+    private fun makeMarker() {
+        mapViewModel.covidDataList.observe(this) {
+            it.forEach { item ->
+                val marker = Marker()
+                when (item.centerType) {
+                    area -> marker.icon =
+                        OverlayImage.fromResource(R.drawable.ic_baseline_place_24)
+                    region -> marker.icon =
+                        OverlayImage.fromResource(R.drawable.ic_baseline_place_25)
+                }
+                marker.position = LatLng(item.lat.toDouble(), item.lng.toDouble())
+                marker.map = naverMap
+                alertMarkerData(marker, item)
+            }
         }
-        marker.map = naverMap
+    }
+
+    private fun alertMarkerData(marker: Marker, item: CovidEntity) {
+        marker.setOnClickListener {
+            val addLocationDust = AlertDialog.Builder(this@MapActivity)
+            addLocationDust.setTitle(R.string.vaccinationCenterInformation)
+                .setMessage(
+                    "${R.string.vaccinationCenterName}" + " : ${item.centerName}\n" +
+                            R.string.address + " : ${item.address}\n" +
+                            R.string.phoneNumber + " : ${item.phoneNumber}"
+                )
+                .setNegativeButton(R.string.close, null)
+                .show()
+            return@setOnClickListener true
+        }
+    }
+
+    companion object {
+        const val area = "중앙/권역"
+        const val region = "지역"
     }
 }
