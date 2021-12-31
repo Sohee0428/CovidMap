0ce7a848140ca38cbb78ae63d45e589d0c497de4
Sohee0428 <82200065+Sohee0428@users.noreply.github.com>
2021-12-31 20:20:00 +0900

    remoteData

--- app/src/main/java/com/autocrypt/covidmap/splash/repository/remote/CovidRemoteDataSource.kt
+++ app/src/main/java/com/autocrypt/covidmap/splash/repository/remote/CovidRemoteDataSource.kt
@@ -1,9 +1,9 @@
 package com.autocrypt.covidmap.splash.repository.remote
 
-import com.autocrypt.covidmap.data.response.CovidInquiryResponse
+import com.autocrypt.covidmap.data.response.CovidVaccinationResponse
 import com.autocrypt.covidmap.data.response.RequestCovidInquiry
 import retrofit2.Callback
 
 interface CovidRemoteDataSource {
-    fun getCovidInquiryCenter(request: RequestCovidInquiry, callback: Callback<CovidInquiryResponse>)
+    fun getCovidVaccinationCenter(request: RequestCovidInquiry, callback: Callback<CovidVaccinationResponse>)
 }
--- app/src/main/java/com/autocrypt/covidmap/splash/repository/remote/CovidRemoteDataSourceImpl.kt
+++ app/src/main/java/com/autocrypt/covidmap/splash/repository/remote/CovidRemoteDataSourceImpl.kt
@@ -3,18 +3,18 @@ package com.autocrypt.covidmap.splash.repository.remote
 import com.autocrypt.covidmap.App
 import com.autocrypt.covidmap.R
 import com.autocrypt.covidmap.data.network.RetrofitCreator
-import com.autocrypt.covidmap.data.response.CovidInquiryResponse
+import com.autocrypt.covidmap.data.response.CovidVaccinationResponse
 import com.autocrypt.covidmap.data.response.RequestCovidInquiry
 import retrofit2.Callback
 
 class CovidRemoteDataSourceImpl : CovidRemoteDataSource {
     private val covidInquiryService = RetrofitCreator.create()
 
-    override fun getCovidInquiryCenter(
+    override fun getCovidVaccinationCenter(
         request: RequestCovidInquiry,
-        callback: Callback<CovidInquiryResponse>
+        callback: Callback<CovidVaccinationResponse>
     ) {
-        covidInquiryService.getCovidInquiryCenter(
+        covidInquiryService.getCovidVaccinationCenter(
             request.perPage,
             request.page,
             App.instance.getString(R.string.SERVICE_KEY)
