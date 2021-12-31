6d854941f81ccd999c4674e47e92f93d2d6eb5a9
Sohee0428 <82200065+Sohee0428@users.noreply.github.com>
2021-12-31 20:17:00 +0900

    Loading 화면 만들기 10개의 데이터를 10페이지씩 불러 룸에 저장하기 저장 후 지도 화면으로 넘기기

---
+++ app/src/main/java/com/autocrypt/covidmap/splash/LoadingActivity.kt
@@ -0,0 +1,34 @@
+package com.autocrypt.covidmap.splash
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.autocrypt.covidmap.R
+import com.autocrypt.covidmap.databinding.ActivityLoadingBinding
+import com.autocrypt.covidmap.map.MapActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class LoadingActivity : AppCompatActivity() {
+
+    private val loadingViewModel: LoadingViewModel by viewModels()
+    private lateinit var binding: ActivityLoadingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_loading)
+
+        loadingViewModel.startMapActivityEvent.observe(this) {
+            startActivity(Intent(this, MapActivity::class.java))
+            finish()
+        }
+        CoroutineScope(Dispatchers.IO).launch {
+            loadingViewModel.deleteAll()
+            loadingViewModel.getCovidInquiryCenter()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/autocrypt/covidmap/splash/LoadingViewModel.kt
@@ -0,0 +1,66 @@
+package com.autocrypt.covidmap.splash
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.autocrypt.covidmap.data.entity.CovidEntity
+import com.autocrypt.covidmap.data.response.CovidVaccinationResponse
+import com.autocrypt.covidmap.data.response.RequestCovidInquiry
+import com.autocrypt.covidmap.splash.repository.CovidRepository
+import com.autocrypt.covidmap.splash.repository.CovidRepositoryImpl
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class LoadingViewModel : ViewModel() {
+    private val repository: CovidRepository = CovidRepositoryImpl()
+    private var page = 1
+
+    private val _startMapActivityEvent = MutableLiveData<Unit>()
+    val startMapActivityEvent: LiveData<Unit>
+        get() = _startMapActivityEvent
+
+    suspend fun getCovidInquiryCenter() {
+        val request = RequestCovidInquiry(page, defaultPerPage)
+        val callback = object : Callback<CovidVaccinationResponse> {
+            override fun onResponse(
+                call: Call<CovidVaccinationResponse>,
+                response: Response<CovidVaccinationResponse>
+            ) {
+                val entityList = CovidVaccinationResponse.toEntityList(response.body()!!)
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    insertDB(entityList)
+                }
+                if (page == 10) {
+                    _startMapActivityEvent.value = Unit
+                    CoroutineScope(Main).launch {
+                    }
+                } else {
+                    page++
+                    CoroutineScope(Dispatchers.IO).launch { getCovidInquiryCenter() }
+                }
+            }
+
+            override fun onFailure(call: Call<CovidVaccinationResponse>, t: Throwable) {
+            }
+        }
+        repository.getCovidVaccinationCenter(request, callback)
+    }
+
+    suspend fun insertDB(entityList: List<CovidEntity>) {
+        repository.insertAll(entityList)
+    }
+
+    suspend fun deleteAll() {
+        repository.deleteAll()
+    }
+
+    companion object {
+        const val defaultPerPage = 10
+    }
+}
